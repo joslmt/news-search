d32a2e04aa547e03c67e8d4ec0007e722d85f687
joslmt <64318244+joslmt@users.noreply.github.com>
2021-01-07 16:54:57 +0100

    v1

---
+++ news.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+    const buscarNoticia = document.querySelector("#buscarNoticia");
+    const wrapNoticia = document.querySelector(".zonaNoticias");
+    const tituloN = document.querySelector(".tituloBusqueda");
+
+const obtenerNoticia = () => {
+    const valor = buscarNoticia.value;
+    fetch(`https://bing-news-search1.p.rapidapi.com/news/search?q=${valor}&setLang=Spanish&cc=es-ES&mkt=es-ES&freshness=Day&textFormat=Raw&safeSearch=Off`, {
+        "method": "GET",
+        "headers": {
+            "x-bingapis-sdk": "true",
+            "x-rapidapi-key": "dd5934dbb7msh9131495b2e28ba0p1cbc37jsnc0860d71a641",
+            "x-rapidapi-host": "bing-news-search1.p.rapidapi.com"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        borrarNoticia();
+        noticia(data, valor)
+        buscarNoticia.value = '';
+    })
+    .catch(err => console.error(err) );
+}
+const noticia = (data, valor) => {
+    tituloN.innerHTML =`<h2>Noticias sobre ${valor}</h2>`;
+
+    //Recorremos el array de objetos JSON.
+    data.value.forEach( noticia => wrapNoticia.innerHTML += `
+    <div id='noticia'>
+        <h3>${noticia.name}</h3>
+        <p>"${noticia.description}" ... <a href='${noticia.url}'>Leer noticia completa</a></p>
+        <p id='autor'>${noticia.provider[0].name}</p>
+    </div>`
+    );
+}
+
+    const borrarNoticia = () => { wrapNoticia.innerHTML = ''; }
+
+    buscarNoticia.onchange = obtenerNoticia;
+}
